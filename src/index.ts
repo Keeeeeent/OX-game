05099c24b665e14939ac04986164f620a6bb45c8
keeeeeent <knt@ne.jp>
2022-06-25 17:41:42 +0900

    【追加実装】 ・引き分け判定 ・ゲームリセット機能 ・ターン表示機能 ・使用方法挿入

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import "./index.sass"
 import model from "./model"
 const models = new model()
 models.set_listener()
+
+document.getElementById("reset").addEventListener("click",()=> models.reset())
--- src/model.ts
+++ src/model.ts
@@ -43,8 +43,10 @@ export default class model{
     private change_base():number{
         if (this.turn === 0) {
             this.turn = 1
+            document.getElementById("turn").innerText = "次は　○のターン"
         }else{
             this.turn = 0
+            document.getElementById("turn").innerText = "次は　×のターン"
         }
         return this.turn
     }
@@ -52,21 +54,55 @@ export default class model{
     private game():boolean{
         for (let i = 0; i < 3; i++) {
             if (this.base[i][0] != null && this.base[i][1] === this.base[i][0] && this.base[i][2] === this.base[i][0]) {
+                this.winner_test()
                 return true
             }
         }
         for (let i = 0; i < 3; i++) {
             if (this.base[0][i] != null && this.base[1][i] === this.base[0][i] && this.base[2][i] === this.base[0][i]) {
+                this.winner_test()
                 return true
             }
         }
         if(this.base[0][0] != null && this.base[1][1] === this.base[0][0] && this.base[2][2] === this.base[0][0]){
+            this.winner_test()
             return true
         }
         if(this.base[0][2] != null && this.base[1][1] === this.base[0][2] && this.base[2][0] === this.base[0][2]){
+            this.winner_test()
+            return true
+        }
+        if(this.draw()){
+            document.getElementById("turn").innerText = "引き分け！！"
             return true
         }
         return false
     }
-
+    private winner_test():void{
+        if (this.turn === 0) {
+            document.getElementById("turn").innerText = "○の勝利！！"
+        }else{
+            document.getElementById("turn").innerText = "×の勝利！！"
+        }
+    }
+    private draw():boolean{
+        let null_counter:number = 0
+        for (let i = 0; i < this.base.length; i++) {
+            for (let j = 0; j < this.base[i].length; j++) {
+                if(this.base[i][j] === null){null_counter += 1}
+            }
+        }
+        if(null_counter === 0){return true}
+        return false
+    }
+    public reset():void{
+        this.base = [[null,null,null],[null,null,null],[null,null,null]]
+        for (let i = 0; i < this.base.length; i++) {
+            for (let j = 0; j < this.base[i].length; j++) {
+                document.getElementById(i + "." + j).innerHTML= ""
+            }
+        }
+        this.turn = 0
+        document.getElementById("turn").innerText = "×のターン"
+    }
 }
